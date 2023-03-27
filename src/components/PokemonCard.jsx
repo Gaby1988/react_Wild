ae25e0989398d6594d22a77fc6c32b1f8c3230a7
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-03-27 15:32:41 +0200

    React Basics 02

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,5 @@
-const pokemon = "bulbasaur";
-let i;
-const pokemonList = [
-    {
-      name: pokemon,
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
 function PokemonCard(){
-    if( pokemonList[i].imgSrc ) {
-        i = 0;
-        return <figure><img src={pokemonList[i].imgSrc} alt="" /><figcaption>{pokemonList[i].name}</figcaption></figure>
-    } else {
-        i = 1;
-        return <figure><p>???</p><figcaption>{pokemonList[i].name}</figcaption></figure>
-    }
-
-    
+    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/><figcaption>bulbasaur</figcaption></figure>
 }
-
 
 export default PokemonCard;
